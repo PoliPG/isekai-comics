2fae8f6360c2f4c0520f3e782cc3ae23486a7ff8
PoliPG <hipolito.jose.perez@gmail.com>
2024-03-14 19:14:37 +0100

    cambio dev packages

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import { remarkReadingTime } from './src/utils/readTime.ts'
@@ -23,16 +22,5 @@ export default defineConfig({
       wrap: true,
     },
   },
-  integrations: [
-    mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: {
-        theme: 'material-theme-palenight',
-        wrap: true,
-      },
-      drafts: true,
-    }),
-    sitemap(),
-    tailwind(),
-  ],
+  integrations: [sitemap(), tailwind()],
 })
